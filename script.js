8e92ad1547b6ac6b576c6e80c2589e1592657e3e
Peter Fossey <peter@fossweb.net>
2017-11-07 14:21:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
-
 $(document).ready(function() {
 	var config = {
 		uptimerobot: {
