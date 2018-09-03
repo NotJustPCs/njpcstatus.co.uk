2d2916604eee3a521431dd382e88f8b1acad966a
Peter Fossey <peter@fossweb.net>
2018-09-03 17:31:02 +0100

    JSON and GeoIP changes

--- script_tests.js
+++ script_tests.js
@@ -58,3 +58,23 @@ function MeasureConnectionSpeed() {
 		]);
 	}
 }
+
+function LoadGeoIPVars(JSONdata) {
+	var obj = JSON.parse(JSONdata);
+	var userip = obj.ip;
+}
+
+function getJSONP(url, success) {
+    var ud = '_' + +new Date,
+        script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0] 
+               || document.documentElement;
+
+    window[ud] = function(data) {
+        head.removeChild(script);
+        success && success(data);
+    };
+
+    script.src = url.replace('callback=?', 'callback=' + ud);
+    head.appendChild(script);
+}
