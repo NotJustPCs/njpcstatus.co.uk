afcc97941fba421d46dd5be513616fc8f2b74204
Peter Fossey <peter@fossweb.net>
2018-09-05 16:57:44 +0100

    Update script_tests.js

--- script_tests.js
+++ script_tests.js
@@ -81,7 +81,7 @@ function getJSONP(url, success) {
     script.src = url.replace('callback=?', 'callback=' + ud);
     head.appendChild(script);
 }
-getJSONP('https://api.ipify.org/?format=json&callback=?', function(data){
+getJSONP('https://json.geoiplookup.io/&callback=?', function(data){
     console.log(data);
     LoadGeoIPVars(data);
 });
